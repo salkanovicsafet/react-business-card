005b72def3ba3907435061a69af5db622e591229
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-02 17:32:41 +0100

    Write Info.js css

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,4 +1,5 @@
 import woman from "../assets/images/woman.png";
+import emailLogo from "../assets/images/Mail Icon.png";
 
 export default function Info() {
   return (
@@ -7,6 +8,10 @@ export default function Info() {
       <h1 className="info--name">Laura Smith</h1>
       <h3 className="info--job-title">Frontend Developer</h3>
       laurasmith.website
+      <button className="info--btn">
+        <img src={emailLogo} alt="Email logo" className="info--btn-mail-logo" />
+        Email
+      </button>
     </div>
   );
 }
