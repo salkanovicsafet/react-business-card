dd8910e4bb2b9395361d0524dd83d7bffb6d69fe
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-02 17:00:47 +0100

    Write Info.js html

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,3 +1,12 @@
+import woman from "../assets/images/woman.png";
+
 export default function Info() {
-  return <h1>Info</h1>;
+  return (
+    <div className="info">
+      <img src={woman} alt="Woman" className="info--img" />
+      <h1 className="info--name">Laura Smith</h1>
+      <h3 className="info--job-title">Frontend Developer</h3>
+      laurasmith.website
+    </div>
+  );
 }
