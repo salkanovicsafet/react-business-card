3d3ecb4181966722a5e2d8b7538e2a660fd7d0c7
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-02 18:04:25 +0100

    Write Footer.js html

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,33 @@
+import twitterLogo from "../assets/images/Twitter Icon.png";
+import facebookLogo from "../assets/images/Facebook Icon.png";
+import instagramLogo from "../assets/images/Instagram Icon.png";
+import linkedInLogo from "../assets/images/Linkedin Icon.png";
+import gitHubLogo from "../assets/images/GitHub Icon.png";
+
 export default function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <div className="footer">
+      <img
+        src={twitterLogo}
+        alt="Twitter logo"
+        className="footer--twitter-logo"
+      />
+      <img
+        src={facebookLogo}
+        alt="Facebook logo"
+        className="footer--facebook-logo"
+      />
+      <img
+        src={instagramLogo}
+        alt="Instagram logo"
+        className="footer--instagram-logo"
+      />
+      <img
+        src={linkedInLogo}
+        alt="LinkedIn logo"
+        className="footer--linkedIn-logo"
+      />
+      <img src={gitHubLogo} alt="GitHub logo" className="footer--github-logo" />
+    </div>
+  );
 }
