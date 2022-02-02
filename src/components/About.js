ba42267805f96c5b5bc99bb5599643dd22cd642e
Safet Salkanović <salkanovicsafet@gmail.com>
2022-02-02 17:36:26 +0100

    Write About.js html

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,10 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <div className="about">
+      <h2>About</h2>I am a frontend developer with a particular interest in
+      making things simple and automating daily tasks. I try to keep up with
+      security and best practices, and am always looking for new things to
+      learn.
+    </div>
+  );
 }
